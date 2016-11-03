4edb3ae95b673f0c8d581e830084f3ab0c0a88f4
Tamara Temple <tamouse@gmail.com>
2016-11-03 05:35:34 -0500

    Create gulpfile to run sass conversions

---
+++ practice/gulpfile.js
@@ -0,0 +1,35 @@
+// task runner -- use this on Windows
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var help = require('gulp-task-listing');
+var browserSync = require('browser-sync');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+   return gulp.src(
+       [
+           'sass/**/*.scss',
+           'sass/**/*.sass'
+       ])
+       .pipe(sourcemaps.init())
+       .pipe(sass().on('error', sass.logError))
+       .pipe(sourcemaps.write())
+       .pipe(gulp.dest('css'))
+       .pipe(browserSync.stream())
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('serve', ['build'], function(){
+    browserSync.init({
+        server: '.'
+    });
+    gulp.watch([
+            './**.*'
+        ],
+        ['build'])
+});
+gulp.task('default', ['serve']);
+gulp.task('help', help);
